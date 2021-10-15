1e78d599de6479486ebf16f07aa7b45c37401b70
pedro-7123 <92324599+pedro-7123@users.noreply.github.com>
2021-10-15 18:41:28 -0300

    Update commands.h
    Nomes dos tipos padronizados

--- src/commands.h
+++ src/commands.h
@@ -1,9 +1,9 @@
-///             Arquitetura e OrganizaÁ„o de Computadores II
+///             Arquitetura e Organiza√ß√£o de Computadores II
 ///                   Trabalho 2: Light File System
 ///
 ///             Alunos:
 ///                     (00326477)  Felipe Kaiser Schnitzler
-///                     (00323741)  NÌkolas Pad„o
+///                     (00323741)  N√≠kolas Pad√£o
 ///                     (00275960)  Pedro Afonso Tremea Serpa
 ///                     (00xxxxxx)  Ricardo
 
@@ -11,7 +11,7 @@
 
 struct type_command;
 
-typedef struct type_args
+typedef struct Type_Args
 {
     struct type_command *owner; //o comando real que bate com oque o usuario digitou
     char *args;                 //argumentos do comando que usuario digitou
@@ -20,7 +20,7 @@ typedef struct type_args
     unsigned char cluster_atual; //a partir de que cluster executar as funcoes
 } Arguments;
 
-typedef struct type_command
+typedef struct Type_Command
 {
     char name[10];               //nome de comando aceito pelo terminal
     unsigned int expected_args; //numero de args que o comando vai requerir
@@ -28,7 +28,7 @@ typedef struct type_command
 } Command;
 
 
-//sÛ teste das funcoes, nao implementar elas aqui, so colocar header
+//s√≥ teste das funcoes, nao implementar elas aqui, so colocar header
 int CD_function(Arguments *arguments);
 int DIR_function(Arguments *arguments);
 int RM_function(Arguments *arguments);
