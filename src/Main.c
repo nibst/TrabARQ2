ca72ca786e8bfb4a74c1f5c2e3a4cb5123b62dc8
pedro-7123 <92324599+pedro-7123@users.noreply.github.com>
2021-10-22 21:00:18 -0300

    a main agora aceita comandos em minusculo e também tem um comando novo pra encerrar o programa "exit"

--- src/Main.c
+++ src/Main.c
@@ -1,9 +1,9 @@
-///             Arquitetura e Organização de Computadores II
+///             Arquitetura e Organiza��o de Computadores II
 ///                   Trabalho 2: Light File System
 ///
 ///             Alunos:
 ///                     (00326477)  Felipe Kaiser Schnitzler
-///                     (00323741)  Níkolas Padão
+///                     (00323741)  N�kolas Pad�o
 ///                     (00275960)  Pedro Afonso Tremea Serpa
 ///                     (00xxxxxx)  Ricardo
 
@@ -12,7 +12,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #define CD 0
 #define DIR 1
 #define RM 2
@@ -24,7 +23,7 @@
 #define ROOTNAME "root"
 #define MAX_INSTRUCTION_SIZE 1000
 
-//só os espaços em branco da esquerda
+//s� os espa�os em branco da esquerda
 char* leftTrim(char *args)
 
 {
@@ -35,8 +34,7 @@ char* leftTrim(char *args)
     }
     return args;
 }
-
-//tira os espaçoes em branco da direita
+//tira os espa�oes em branco da direita
 char *rightTrim(char *args)
 {
     int i = strlen(args)-1;
@@ -47,7 +45,6 @@ char *rightTrim(char *args)
     }
     return args;
 }
-
 char* trim(char *args)
 {
     args = leftTrim(args);
@@ -68,7 +65,7 @@ int countArguments(char *args)
         while((args[i]!= ' ') && (args[i]!= '\0'))
             i++;
         //passa pelos espaços em branco entre argumentos
-        //não precisa testar '\0' pq args ja vai estar trimmed então não existe
+        //não precisa testar '\0' pq args ja vai estar trimmed ent�o n�o existe
         //como ter ' ' seguido de '\0'
         while((args[i]) == ' ')
             i++;
@@ -101,7 +98,7 @@ Arguments get_command_and_args(char *instruction_line, Arguments instruction)
             instruction.args[strlen(instruction.args) - 1] = '\0';
        instruction.args = trim(instruction.args);
        instruction.num_args = countArguments(instruction.args);
-        //!fazer uma funcao para contar os argumentos, considerar argumento entre aspas como um só -> comando editar
+        //!fazer uma funcao para contar os argumentos, considerar argumento entre aspas como um s� -> comando editar
     }
     //free(instruction_line_copy);
     return instruction;
@@ -109,34 +106,49 @@ Arguments get_command_and_args(char *instruction_line, Arguments instruction)
 
 void emulaCMD()
 {
-    char *dirName = ROOTNAME;
+    char *dirName = (char *)malloc((sizeof(char) * strlen(ROOTNAME)) + 1);//para armazenar ROOTNAME
+    strcpy(dirName,ROOTNAME);
     char *instruction_line = (char *)malloc((sizeof(char) * MAX_INSTRUCTION_SIZE) + 1) ;
     int i=-1;
     BYTE ok = 1;//se rodou o comando corretamente = 0,senao = 1
     Arguments instruction;
     instruction.cluster_atual = 0x00;
-    
-    while (1)
+
+    int ex = 0;
+    while (!ex)
     {
-        if(ok == 0 && i == CD)
-            dirName = instruction.args;
+
+          if(ok == 0 && i == CD)
+        {
+            free(dirName);
+            dirName = (char *)malloc((sizeof(char) * strlen(instruction.args)) + 1);
+            strcpy(dirName,instruction.args);
+        }
 
         printf("%s",dirName);
         printf(">");
         fgets(instruction_line, MAX_INSTRUCTION_SIZE, stdin);
-        
         //se o primeiro caractere não for enter
         if (instruction_line[0] != 10)
         {
            instruction = get_command_and_args(instruction_line,instruction);
+
+           int p;   ///Pra aceitar letra minuscula tbm.
+           for (p=0; instruction.command_name[p] != '\0'; p++)
+           {
+               instruction.command_name[p] = toupper(instruction.command_name[p]);
+           }
+
             for(i =0; i<NCOMMANDS; i++)
             {
+
+
+
                 //se comando digitado tiver na lista de comandos la
                 if(!strcmp(commands[i].name,instruction.command_name))
                 {
                     instruction.owner = &commands[i];
                     //roda funcao daquele comando
-                    
                     ok = (commands[i].func)(&instruction);
                     //a instruction_line_copy da funcao acima vira instruction.command_name na funcao acima
                     //tem que dar free nela.
@@ -157,9 +169,9 @@ int main()
 
     //inicializar a estrutura de arquivos de dados
     //emular o cmd, fazer coisas tipo root\dir> <tal comando>
-    fileSystem *arq = (fileSystem *)malloc(sizeof(fileSystem));
+    FileSystem *arq = (FileSystem *)malloc(sizeof(FileSystem));
     inicializaArquivo(arq);
-
+    free(arq);
 
     emulaCMD();
 
